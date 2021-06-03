614c6033a604246496951370f53e8c129bd4c34a
liam0999 <74009289+liam0999@users.noreply.github.com>
2021-06-03 18:30:16 -0400

    Test commit

--- games.lua
+++ games.lua
@@ -1,5 +1,9 @@
 return {
 
-    ["a literal baseplate."] = 4483381587;
+    ["a literal baseplate."] = {
+        id = 4483381587;
+        desc = "Testing place, no actual features besides universal.";
+        info = "Information tab, testing info.";
+    };
     
 }
