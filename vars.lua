44433cf60c123e82e633f95e70695bec8a688a6d
liam0999 <74009289+liam0999@users.noreply.github.com>
2021-06-05 20:49:13 -0400

    vars

--- vars.lua
+++ vars.lua
@@ -1,9 +1,12 @@
 local genv = getgenv()
 local vars = {
+    RunService = game:GetService("RunService");
     ReplicatedStorage = game:GetService("ReplicatedStorage");
     TweenService = game:GetService("TweenService");
     TextService = game:GetService("TextService");
     UserInputService = game:GetService("UserInputService");
+    MarketplaceService = game:GetService("RunService");
+    TeamService = game:GetService("Teams");
     HttpService = game:GetService("HttpService");
     Players = game:GetService("Players");
     LocalPlayer = game:GetService("Players").LocalPlayer;
